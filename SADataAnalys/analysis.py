c0d93e4cace77f3b049fc70db52ea10baaf8244b
garok <garokuo@gmail.com>
2024-04-08 18:39:10 -0700

    Contact GPT-4 API

--- SADataAnalys/analysis.py
+++ SADataAnalys/analysis.py
@@ -1,3 +1,63 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a9a471a974664d54e9bc365b7e43e3e53d8fd029e5143ef5b3cd38d552ec4020
-size 1677
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import numpy as np 
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+def sentimentAnalysis(undetermined):
+    undetermined_static = undetermined
+    nltk.download('punkt')
+    #Start with text processing
+    #Lower and remove punctuation
+    undetermined = [sentence.lower().translate(str.maketrans('', '', string.punctuation)) for sentence in undetermined]
+
+    #Remove stop words
+    nltk.download('stopwords')
+    stop_words = set(stopwords.words('english'))
+    undetermined = [' '.join([word for word in word_tokenize(sentence) if word.lower() not in stop_words]) for sentence in undetermined]
+
+    #Lemmatizeation
+    nltk.download('wordnet')
+    lemmatizer = WordNetLemmatizer()
+    undetermined = [' '.join([lemmatizer.lemmatize(word) for word in word_tokenize(sentence)]) for sentence in undetermined]
+
+    #Tokenizer
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle) 
+    undetermiend_sequences = tokenizer.texts_to_sequences(undetermined) 
+
+    #Pad sequences
+    max_length = 15
+    padded_sequences = pad_sequences(undetermiend_sequences, maxlen=max_length, padding='post')
+    
+    #Load ML model
+    model = load_model('sentiment_analysis_ML')
+
+    #Predict rating
+    predictions = model.predict(padded_sequences)
+
+    #Test print
+    #print(predictions)
+
+    #Determine sentiment and classify
+    predictions_list = predictions.tolist()
+    pos = []
+    neg = []
+    for i in range(len(predictions_list)):
+        if predictions_list[i][0] > 0.5:
+            pos.append(undetermined_static[i])
+        else:
+            neg.append(undetermined_static[i])
+
+    return pos, neg
--- SADataAnalys/grab_data.py
+++ SADataAnalys/grab_data.py
@@ -1,3 +1,76 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5feb079ed3849dee7b2269aa84aea9ae6db65db858590b4b2e9a4f00e1b1e936
-size 1690
+from jotform import *
+from openai import OpenAI #python3.10 -m pip install openai @gigaComputer
+
+import re
+import os
+from dotenv import load_dotenv #pip install python-dotenv @gigaComputer
+load_dotenv() #.env contains OPENAI_API_KEY variable (check Discord)
+
+#File import
+import analysis
+from queries import query, role
+
+def main():
+    jotformAPIClient = JotformAPIClient('0698fe80cbad87f1efa0d63ba86a63e5')
+
+    submissions = jotformAPIClient.get_form_submissions('240840940974159')
+
+    question = 'Student FIRST NAME (Please ensure correct spelling and capitalization)'
+    student = 'Andrew' #Andrew for test
+
+    positive = []
+    negative = []
+    undetermined = []
+
+    for submission in submissions:
+        answers = submission.get('answers', {})
+        for key, value in answers.items():
+            if value['text'] == question and value['answer'] == student:
+                clean_response = {k: v['answer'] for k, v in answers.items() if 'answer' in v}
+
+                for key, value in clean_response.items():
+                    if key == '16':
+                        undetermined.append(value)
+                    elif key == '17':
+                        positive.append(value)
+                    elif key == '18':
+                        negative.append(value)
+    positive_sentences = [sentence.strip() for text in positive for sentence in filter(None, re.split(r'(?<!\w\.\w.)(?<![A-Z][a.z]\.)(?<=\.|\?|\!)\s', text))]
+    negative_sentences = [sentence.strip() for text in negative for sentence in filter(None, re.split(r'(?<!\w\.\w.)(?<![A-Z][a.z]\.)(?<=\.|\?|\!)\s', text))]
+    undetermined_sentences = [sentence.strip() for text in undetermined for sentence in filter(None, re.split(r'(?<!\w\.\w.)(?<![A-Z][a.z]\.)(?<=\.|\?|\!)\s', text))]
+    
+    print(positive_sentences)
+    print(negative_sentences)
+    print(undetermined_sentences)
+
+    pos, neg = analysis.sentimentAnalysis(undetermined_sentences)
+
+    print(f'Positive: {pos}')
+    print(f'Negative: {neg}')
+
+    positive_sentences.extend(pos)
+    negative_sentences.extend(neg)
+
+    client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+    system_message = {
+        'role': 'system',
+        'content': role
+    }
+
+    behaviors = [
+        {'role': 'user', 'content': query}
+        #{'role' : 'user', 'content' : f'Positive behaviors: {positive_sentences}'}
+        #{'role' : 'user', 'content' : f'Negative behaviors: {negative_sentences}'}
+        #{'role' : 'user', 'content' : f'Background information: {background_information}'}
+    ]
+
+    completion = client.chat.completions.create(
+        model = 'gpt-4-turbo-preview',
+        messages = [system_message] + behaviors
+    )
+
+    print(completion.choices[0].message.content)
+
+if __name__ == '__main__':
+    main()
